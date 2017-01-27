85dec21560f871278e8f2177dc6616feddd2df85
zorn93 <olivier93170@hotmail.fr>
2017-01-27 09:55:54 +0100

    Test Unitaire Java me manque Testadd et Remove

--- InscriptionsSportives/src/junit/CompetitionTest.java
+++ InscriptionsSportives/src/junit/CompetitionTest.java
@@ -1,10 +1,16 @@
-package tests;
+package junit;
 
 import static org.junit.Assert.*;
+
+import java.time.LocalDate;
 
 import org.junit.Test;
 
 import inscriptions.Competition;
+import inscriptions.Equipe;
+import inscriptions.Inscriptions;
+import inscriptions.Personne;
+
 import static inscriptions.Inscriptions.*;
 
 public class CompetitionTest {
@@ -25,63 +31,143 @@ public class CompetitionTest {
 
 	@Test
 	public void testSetNom() {
-		
-		fail("Not yet implemented");
+		inscriptions.Inscriptions inscriptions = getInscriptions();
+		Competition compet = inscriptions.createCompetition("gertrude", null, true);
+		compet.setNom("gertrude");
+		assertEquals("gertrude", compet.getNom());
 	}
 
 	@Test
 	public void testInscriptionsOuvertes() {
-		fail("Not yet implemented");
+		
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition compet = inscriptions.createCompetition("Thresh", LocalDate.now().plusDays(3), true);
+		assertTrue(compet.inscriptionsOuvertes());
 	}
 
 	@Test
 	public void testGetDateCloture() {
-		fail("Not yet implemented");
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition compet = inscriptions.createCompetition("test", LocalDate.now(), false);	
+		assertEquals(LocalDate.now(),compet.getDateCloture());
 	}
 
 	@Test
 	public void testEstEnEquipe() {
-		fail("Not yet implemented");
+		
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition compet = inscriptions.createCompetition("Thresh", LocalDate.now().plusDays(20), true);	
+		assertEquals(true,compet.estEnEquipe());
+		
 	}
 
 	@Test
 	public void testSetDateCloture() {
-		fail("Not yet implemented");
+		LocalDate date = LocalDate.now().plusDays(10);
+		LocalDate  date1 = LocalDate.now().plusDays(20);
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition compet = inscriptions.createCompetition("bip", date, true);
+		compet.setDateCloture(date1);
+		assertEquals(date1,compet.getDateCloture());
 	}
+	
 
 	@Test
 	public void testGetCandidats() {
-		fail("Not yet implemented");
-	}
+		Inscriptions inscri = Inscriptions.getInscriptions();
+		Competition compet = inscri.createCompetition("testCompet", LocalDate.now().plusDays(20), false);
+		Personne Olivier = inscri.createPersonne("ha", "oh", "eh");
+		Personne Thresh = inscri.createPersonne("ha", "oh", "eh");
+		compet.add(Olivier);
+		compet.add(Thresh);
+		assertTrue(compet.getCandidats().contains(Olivier));
+		assertTrue(compet.getCandidats().contains(Thresh));
+		}
 
 	@Test
 	public void testAddPersonne() {
-		fail("Not yet implemented");
+		
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		LocalDate date = LocalDate.now().plusDays(20);
+		Competition compet = inscriptions.createCompetition("test", date, false);
+		Personne personne = inscriptions.createPersonne("test", "prenom", "mail");
+		int sizeBefore = compet.getCandidats().size();
+		compet.add(personne);
+		int sizeAfter = compet.getCandidats().size();
+		assertTrue(compet.getCandidats().contains(personne));
+		assertEquals(sizeBefore+1,sizeAfter);
 	}
 
 	@Test
 	public void testAddEquipe() {
-		fail("Not yet implemented");
+		
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		LocalDate date = LocalDate.now().plusDays(20);
+		Competition compet = inscriptions.createCompetition("test", date, true);
+		
+		Personne p = inscriptions.createPersonne("test", "prenom", "mail");
+		Personne pp = inscriptions.createPersonne("test", "prenom", "mail");
+		Personne ppp = inscriptions.createPersonne("test", "prenom", "mail");
+		Equipe equipe = inscriptions.createEquipe("testTeam");
+		Equipe equipe2 = inscriptions.createEquipe("testTeam");
+
+		equipe.add(p);
+		equipe.add(pp);
+		equipe2.add(ppp);
+		
+		compet.add(equipe);
+		compet.add(equipe2);
+		int sizeBefore = inscriptions.getCandidats().size();
+		Equipe eee = inscriptions.createEquipe("Thresh");
+		Personne Personne = inscriptions.createPersonne("test", "test", "mail");
+		eee.add(Personne);
+		compet.add(eee);
+		assertTrue(inscriptions.getCandidats().contains(eee));
+		int sizeAfter = inscriptions.getCandidats().size();
+		assertEquals(sizeBefore+1,sizeAfter);
+
 	}
 
 	@Test
 	public void testRemove() {
-		fail("Not yet implemented");
+		Inscriptions inscri = Inscriptions.getInscriptions();
+		LocalDate date = LocalDate.now().plusDays(20);
+		Competition compet = inscri.createCompetition("test", date,false);
+		Personne Personne = inscri.createPersonne("nom", "prenom", "mail");
+		Personne Personne2 = inscri.createPersonne("nom", "prenom", "mail");
+		compet.add(Personne);
+		compet.add(Personne2);
+		int sizeBefore = compet.getCandidats().size();
+		compet.remove(Personne);
+		int sizeAfter = compet.getCandidats().size();
+		assertEquals(sizeBefore-1,sizeAfter);
 	}
 
 	@Test
 	public void testDelete() {
-		fail("Not yet implemented");
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition compet = inscriptions.createCompetition("Compet", LocalDate.now().plusDays(20), false);
+		int size = inscriptions.getCompetitions().size();
+		compet.delete();
+		assertEquals(size-1,inscriptions.getCompetitions().size());
 	}
 
 	@Test
 	public void testCompareTo() {
-		fail("Not yet implemented");
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition compet = inscriptions.createCompetition("Compet", LocalDate.now().plusDays(20), false);
+		Competition compet2 = inscriptions.createCompetition("Compet", LocalDate.now().plusDays(20), false);
+
+		assertEquals(0,compet.compareTo(compet2));
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition compet = inscriptions.createCompetition("compet", LocalDate.now().plusDays(20), false);
+		assertNotNull(compet.toString());
 	}
 
 }
+	
