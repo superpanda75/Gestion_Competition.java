3590c0224bfd47718020059c1c09d9514df65c24
abigaelle123 <abigaellehouri@gmail.com>
2017-02-03 11:23:51 +0100

    REQUETE PERSONNE + EQUIPE dans la base de donnee

---
+++ InscriptionsSportives/src/jdbc/BasePersonne.java
@@ -0,0 +1,10 @@
+package jdbc;
+
+import inscriptions.Inscriptions;
+
+public class BasePersonne {
+	
+	public static void ListePersonne(String nom, String prenom, String mail) {
+		String AfficherPersonne =" ( "+nom+" , "+prenom+", "+mail+" )";
+	}
+}
