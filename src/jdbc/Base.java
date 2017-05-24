29b4615cf5881701718a1af9aa92358b0bcbd3fb
abigaelle123 <abigaellehouri@gmail.com>
2017-05-24 15:47:38 +0200

    base§.java corrigé fonctionnel

---
+++ src/jdbc/Base.java
@@ -0,0 +1,43 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+	
+public class Base {
+
+	public static Connection c;
+	
+	public Base()
+	{
+		
+	}
+	
+	//-> fonctionne la BDD dans le serveur M2L et local
+	public static Connection connexion() throws SQLException
+	{
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			//jdbc:mysql://localhost:3306/java", "root", ""
+			//jdbc:mysql://mysql.m2l.local/ahouri", "ahouri", "azerty"
+			c = DriverManager.getConnection("jdbc:mysql://mysql.m2l.local/ahouri", "ahouri", "azerty");	
+			System.out.println("Connexion bien établie");
+		
+			
+		}
+		catch (ClassNotFoundException e) {
+			System.out.println("Pilote JDBC non intallé" );
+		}
+		
+		return c;
+	}
+
+	
+
+	public static void main(String[] args) throws SQLException {
+		
+	}
+}
