942e7bc63463789ee81cf2af54c0d04e68218f4a
zorn93 <olivier93170@hotmail.fr>
2017-06-11 00:51:53 +0200

    IhmEquipe

--- src/ihmSwing/IhmEquipe.java
+++ src/ihmSwing/IhmEquipe.java
@@ -1,247 +1,428 @@
 package ihmSwing;
 
-import inscriptions.Inscriptions;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.KeyEvent;
 import java.awt.*;
 
-import java.util.ArrayList;
-import java.util.SortedSet;
+import java.util.Map;
+import java.util.TreeMap;
 
-import java.util.ArrayList;
-
+import javax.swing.event.ListDataListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import javax.swing.event.TableModelListener;
-import javax.swing.table.AbstractTableModel;
 
 import javax.swing.*;
-import net.miginfocom.swing.MigLayout;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
 
-import inscriptions.Candidat;
-import inscriptions.Personne;
+import inscriptions.Inscriptions;
 
-import javax.swing.border.LineBorder;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeEvent;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ActionEvent;
+import inscriptions.Equipe;
 
-public class IhmEquipe implements ItemListener
+
+
+public class IhmEquipe// implements ItemListener
 	{
-		JPanel				cards;
-		private JPanel		ongletEquipe		= new JPanel();
-		private JTabbedPane	menuEquipe	= new JTabbedPane(SwingConstants.RIGHT);
-		private JPanel		panelDroite		= new JPanel();
-		private JTextField	modifNomCand;
+		
+		private JTextField	modifNomEqui	= mkTfNomEqui();
+		private JTextField	modifMembres	= mkTfMembres();
+		private JTextField	modifNomPers	= mkTfNom();
+		
+		private JLabel lblNomEqui 	= mkLbNomEqui();
+		private JLabel lblMembres 	= mkLbMembres();
+		private JLabel lblNom		= mkLbNomPers();
+		
+		private final Inscriptions inscriptions ; 
+		private Map<Integer, Equipe> mapEquipe;
 
-		public IhmEquipe(JLabel titreOnglet)
+		public IhmEquipe(Inscriptions inscriptions)
 		{
-			this.ongletEquipe.setLayout(new BorderLayout());
-			this.ongletEquipe.add(titreOnglet);
-			addComponentToPane(ongletEquipe);
-			// itemStateChanged(event);
-			// this.ongletPers.add(panelDroite,BorderLayout.EAST);
-
+			this.inscriptions = inscriptions;
+		}
+		
+		private void associateLabels(){
+			lblNomEqui.setLabelFor(modifNomEqui);
+			lblMembres.setLabelFor(modifMembres);
+			lblNom.setLabelFor(modifNomPers);
+		}
+		
+		private JLabel mkLbListEqui(){
+			JLabel lblListeDesEquipes = new JLabel("Liste des personnes enregistr\u00E9es :");
+			lblListeDesEquipes.setFont(new Font("Bookman Old Style", Font.BOLD, 14));
+			lblListeDesEquipes.setBounds(37, 43, 249, 16);
+		return lblListeDesEquipes;
+		}
+		
+		private JLabel mkLbNomPers(){
+			JLabel lbl_nom_Equi = new JLabel("Nom de candidat :");
+			lbl_nom_Equi.setForeground(new Color(102, 102, 102));
+			lbl_nom_Equi.setFont(new Font("Bookman Old Style", Font.BOLD, 14));
+			lbl_nom_Equi.setBounds(409, 80, 181, 14);
+		return lbl_nom_Equi;
+		}
+		
+		private JLabel mkLbMembres(){
+			JLabel lbl_Membres = new JLabel("Pr\u00E9nom :");
+			lbl_Membres.setForeground(new Color(102, 102, 102));
+			lbl_Membres.setFont(new Font("Bookman Old Style", Font.BOLD, 14));
+			lbl_Membres.setBounds(409, 174, 181, 14);
+		return lbl_Membres;
+		}
+		
+		private JLabel mkLbNomEqui(){
+			JLabel lbl_Nom_Equi = new JLabel("email :");
+			lbl_Nom_Equi.setForeground(new Color(102, 102, 102));
+			lbl_Nom_Equi.setBackground(new Color(0, 0, 0));
+			lbl_Nom_Equi.setFont(new Font("Bookman Old Style", Font.BOLD, 14));
+			lbl_Nom_Equi.setBounds(409, 265, 181, 14);
+		return lbl_Nom_Equi;
+		}
+		
+		private JTextField mkTfNomEqui(){
+			modifNomEqui = new JTextField();
+			modifNomEqui.setEnabled(false);
+			modifNomEqui.setEditable(false);
+			modifNomEqui.setBackground(new Color(102, 102, 102));
+			modifNomEqui.setBounds(406, 287, 160, 20);
+			modifNomEqui.setColumns(10);
+		return modifNomEqui;
 		}
 
-		public void addComponentToPane(Container pane)
-		{
-			// Put the JComboBox in a JPanel to get a nicer look.
-			JPanel ActionEquipe = new JPanel(); // use FlowLayout
-			ActionEquipe.setBackground(new Color(51, 102, 153));
-			String optionAjouter = "Nouvelle Equipe";
-			String optionModifier = "Modifier une Equipe";
-
-			String comboBoxItems[] =
-			{ optionAjouter, optionModifier };
-			JComboBox cb = new JComboBox(comboBoxItems);
-			cb.setEditable(false);
-			cb.addItemListener(this);
-			ActionEquipe.add(cb);
-
-			// TODO : ici, il serait intéréssant d'alleger la méthode en
-			// mettant dans une autre méthode la creations des cards!
-
-			// Create the "cards".
-			JPanel listMembre = new JPanel();
-
-			listMembre.setBackground(new Color(51, 153, 204));
-			listMembre.setLayout(null);
-			JLabel lblNom = new JLabel("Nom :");
-			lblNom.setFont(new Font("Bookman Old Style", Font.BOLD, 14));
-			lblNom.setBounds(161, 32, 53, 18);
-			listMembre.add(lblNom);
-			JTextField champ_nom = new JTextField(20);
-			champ_nom.setBackground(new Color(51, 102, 153));
-			champ_nom.setBounds(264, 32, 120, 20);
-			listMembre.add(champ_nom);
-
-			JPanel card2 = new JPanel();
-			card2.setBackground(new Color(51, 153, 204));
-
-			JPanel card3 = new JPanel();
-			card3.add(new JTextField("TextField", 20));
-			card3.add(new JButton("Supprimer"));
-
-			// Create the panel that contains the "cards".
-			cards = new JPanel(new CardLayout());
-			cards.add(listMembre, optionAjouter);
-			JButton button = new JButton("Ajouter");
-			button.setBounds(264, 353, 120, 23);
-			listMembre.add(button);
-			cards.add(card2, optionModifier);
-			card2.setLayout(null);
-
-			JLabel lblListeDesPersonnes = new JLabel("Liste des candidats enregistr\u00E9es :");
-			lblListeDesPersonnes.setFont(new Font("Bookman Old Style", Font.BOLD, 14));
-			lblListeDesPersonnes.setBounds(37, 100, 249, 16);
-			listMembre.add(lblListeDesPersonnes);
-			
-			JList list = new JList();
-			list.setBounds(47, 127, 196, 185);
-			listMembre.add(list);
-			
-			JButton btnListRight = new JButton("New Button");
-			btnListRight.setEnabled(false);
-			btnListRight.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent arg0) {
-				}
-			});
-			btnListRight.setBounds(264, 173, 120, 41);
-			listMembre.add(btnListRight);
-			
-			JButton btnListLeft = new JButton("New Button");
-			btnListLeft.setEnabled(false);
-			btnListLeft.setBounds(264, 236, 120, 41);
-			listMembre.add(btnListLeft);
-			
-			JList list_2 = new JList();
-			list_2.setBounds(404, 127, 133, 185);
-			listMembre.add(list_2);
-
-			JButton btnRight = new JButton("New Button");
-			btnRight.setEnabled(false);
-			btnRight.setBounds(250, 195, 109, 39);
-			card2.add(btnRight);
-
-			JLabel lbl_nom_cand = new JLabel("Nom de candidat :");
-			lbl_nom_cand.setForeground(new Color(102, 102, 102));
-			lbl_nom_cand.setFont(new Font("Bookman Old Style", Font.BOLD, 14));
-			lbl_nom_cand.setBounds(77, 34, 141, 14);
-			card2.add(lbl_nom_cand);
-
-			modifNomCand = new JTextField();
-			modifNomCand.setBackground(new Color(102, 102, 102));
-			modifNomCand.setEditable(false);
-			modifNomCand.setEnabled(false);
-			lbl_nom_cand.setLabelFor(modifNomCand);
-			modifNomCand.setBounds(253, 32, 106, 20);
-			card2.add(modifNomCand);
-			modifNomCand.setColumns(10);
-
-			JButton btnValiderLaModification = new JButton("Valider ");
-			btnValiderLaModification.setBounds(248, 300, 109, 23);
-			card2.add(btnValiderLaModification);
-
-			Inscriptions ins = inscriptions.Inscriptions.getInscriptions();
-			SortedSet personnes = ins.getPersonnes();
-			System.out.println(personnes);
-			DefaultListModel model = new DefaultListModel<>();
-			JList<Personne> list_1 = new JList<>(model);
-			for (Object personne : personnes)
-			{
-				model.addElement(personne);
-			}
-			list_1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-			list_1.setBackground(new Color(51, 102, 153));
-			list_1.setBounds(37, 83, 181, 253);
-			list_1.setCellRenderer(getCellRenderer());
-			// =====================DECOMMENTER ET REMPLACER LA LISTE D
-			// AfFICHAGE FACTICE====
-			card2.add(list_1);
-			
-			JButton btnLeft = new JButton("New button");
-			btnLeft.setEnabled(false);
-			btnLeft.setBounds(250, 122, 109, 39);
-			card2.add(btnLeft);
-			
-			JList list_3 = new JList();
-			list_3.setBackground(new Color(0, 102, 153));
-			list_3.setEnabled(false);
-			list_3.setBounds(392, 83, 141, 253);
-			card2.add(list_3);
-			
-			JComboBox comboBox = new JComboBox();
-			comboBox.setModel(new DefaultComboBoxModel(new String[] {"Choisir une equipe"}));
-			comboBox.setMaximumRowCount(999);
-			comboBox.setBounds(224, 62, 135, 20);
-			card2.add(comboBox);
-			// ==============================================================================
-			pane.add(ActionEquipe, BorderLayout.PAGE_START);
-			pane.add(cards, BorderLayout.CENTER);
-
-			// LISTE DE TEST
-			DefaultListModel model_test = new DefaultListModel<>();
-			JList<Personne> list_test = new JList<>(model_test);
-			list_test.setBackground(new Color(51, 102, 153));
-			list_test.setBounds(37, 80, 190, 256);
-
-			// card2.add(list_test);
-
-			list_1.addListSelectionListener(new ListSelectionListener() {
-				public void valueChanged(ListSelectionEvent e)
-				{
-					JList source = (JList) e.getSource();
-					Object selection = source.getSelectedValue();
-					
-					Class unknown = selection.getClass();
-					System.out.println(unknown);
-					
-					int id =((Personne) selection).getId();
-					String mail =((Personne) selection).getMail();
-					String prenom =((Personne) selection).getPrenom();
-					String Nom = ((Personne) selection).getNom();
-					System.out.println(selection);
-					System.out.println(id);
-					System.out.println(mail);
-					System.out.println(prenom);
-					System.out.println(Nom);
-					
-				}
-			});
+		private JTextField mkTfMembres(){
+			modifMembres = new JTextField();
+			modifMembres.setEnabled(false);
+			modifMembres.setEditable(false);
+			modifMembres.setBackground(new Color(102, 102, 102));
+			modifMembres.setBounds(406, 197, 160, 20);
+			modifMembres.setColumns(10);
+		return modifMembres;
 		}
+			
+		private JTextField mkTfNom(){
+			modifNomPers = new JTextField();
+			modifNomPers.setBackground(new Color(102, 102, 102));
+			modifNomPers.setEditable(false);
+			modifNomPers.setEnabled(false);
+			modifNomPers.setBounds(406, 105, 160, 20);
+			modifNomPers.setColumns(10);
+			return modifNomPers;
+		}
+			
 
-		private ListCellRenderer<? super Personne> getCellRenderer()
+			
+		public JPanel getCardModif(){
+			
+			JPanel cardPersonnes = new JPanel();
+			cardPersonnes.setBackground(new Color(51, 153, 204));
+			cardPersonnes.setLayout(null);
+			cardPersonnes.add(mkLbListEqui());
+			
+			cardPersonnes.add(modifNomEqui);
+			cardPersonnes.add(lblNomEqui);
+			
+			cardPersonnes.add(modifMembres);
+			cardPersonnes.add(lblMembres);
+			
+			cardPersonnes.add(modifNomPers);
+			cardPersonnes.add(lblNom);		
+			
+			JButton btnValiderLaModification = new JButton("Valider la modification");
+			btnValiderLaModification.setBounds(250, 351, 160, 23);
+			cardPersonnes.add(btnValiderLaModification);
+			
+			cardPersonnes.add(getListPersonne());
+
+		return cardPersonnes;
+		}
+		
+		private ListModel<String> getListPersonneModel()
 		{
-			return new DefaultListCellRenderer() {
+			rafraichirMap();
+			return new ListModel<String>() {
+				
 				@Override
-				public Component getListCellRendererComponent(JList<?> list, Object value, int index,
-						boolean isSelected, boolean cellHasFocus)
+				public void removeListDataListener(ListDataListener l)
 				{
-					Personne p = (Personne) value;
-					Component listCellRendererComponent = super.getListCellRendererComponent(list,
-							p.getId()+"/"+ p.getNom() + "/" + p.getPrenom() + "/" + p.getMail(), index, isSelected, cellHasFocus);
-					return listCellRendererComponent;
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public int getSize()
+				{
+					return mapEquipe.size() + 1;
+				}
+				
+				@Override
+				public String getElementAt(int index)
+				{
+					if (index < mapEquipe.size())
+						return mapEquipe.get(index).getNom();
+					else
+						return "<Ajouter>";
+				}
+				
+				@Override
+				public void addListDataListener(ListDataListener l)
+				{
+					// TODO Auto-generated method stub
+					
 				}
 			};
 		}
-
-		public void itemStateChanged(ItemEvent evt)
+		
+		private void rafraichirMap()
 		{
-			CardLayout cl = (CardLayout) (cards.getLayout());
-			cl.show(cards, (String) evt.getItem());
+			mapEquipe = new TreeMap<>();
+			int i = 0;
+			for (Equipe p : inscriptions.getEquipe())
+				mapEquipe.put(i++, p);
+		}
+		
+
+		private ListSelectionListener getListPersonneSelectionListener()
+		{
+			return new ListSelectionListener() {
+				public void valueChanged(ListSelectionEvent e)
+				{
+					JList<String> source = (JList<String>) e.getSource();
+					int index = source.getSelectedIndex();
+					System.out.println(index);
+					System.out.println(source.getModel().getSize() -1);
+					if (index == source.getModel().getSize() - 1){
+						//gérer l'ajout ici
+						System.out.println("Ajout d'une personne");
+					}
+					else{						
+						//gérer la modif ici
+						activerChampNom(mapEquipe.get(index).getNom());
+						activerChampPrenom(mapEquipe.get(index).getPrenom());
+						activerChampEmail(mapEquipe.get(index).getMail());
+						
+						System.out.println("Selection de " + mapEquipe.get(index).getNom());
+					}
+				}
+			};
+		}
+		
+		private ListSelectionListener getValidatioSelectionListener()
+		{
+			return new ListSelectionListener() {
+				public void valueChanged(ListSelectionEvent e)
+				{
+					JList<String> source = (JList<String>) e.getSource();
+					int index = source.getSelectedIndex();
+					System.out.println(index);
+					System.out.println(source.getModel().getSize() -1);
+					if (index == source.getModel().getSize() - 1){
+						//gérer l'ajout ici
+						System.out.println("Ajout d'une personne");
+					}
+					else{						
+						//gérer la modif ici
+						activerChampNom(mapEquipe.get(index).getNom());
+						activerChampPrenom(mapEquipe.get(index).getPrenom());
+						activerChampEmail(mapEquipe.get(index).getMail());
+						
+						System.out.println("Selection de " + mapEquipe.get(index).getNom());
+					}
+				}
+			};
+		}
+		
+		private JList<String> getListPersonne(){
+			JList<String> listePersonnes = new JList<>(getListPersonneModel());
+			listePersonnes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			listePersonnes.setBackground(new Color(51, 102, 153));
+			listePersonnes.setBounds(37, 80, 190, 256);
+			listePersonnes.addListSelectionListener(getListPersonneSelectionListener());	
+			return listePersonnes;
+			}
+		
+		
+		/**
+		 * Permet d'activer un champ et de le remplir
+		 * @param data
+		 */
+		public void activerChampEmail(String data){
+			modifNomEqui.setText("");
+			modifNomEqui.setEditable(true);
+		    modifNomEqui.setEnabled(true);
+		    modifNomEqui.setBackground(new Color(51, 102, 153));
+		    lblNomEqui.setForeground(new Color(0,0,0));
+		    modifNomEqui.setText(data);	
+		}
+		
+		/**
+		 * Permet d'activer un champ et de le remplir
+		 * @param data
+		 */
+		public void activerChampPrenom(String data){
+			modifMembres.setText("");
+			modifMembres.setEditable(true);
+			modifMembres.setEnabled(true);
+			modifMembres.setBackground(new Color(51, 102, 153));
+		    lblMembres.setForeground(new Color(0,0,0));
+		    modifMembres.setText(data);	
+		}
+		
+		/**
+		 * Permet d'activer un champ et de le remplir
+		 * @param data
+		 */
+		public void activerChampNom(String data){
+			modifNomPers.setEditable(true);
+			modifNomPers.setText("");
+		    modifNomPers.setEnabled(true);
+		    modifNomPers.setBackground(new Color(51, 102, 153));
+		    lblNom.setForeground(new Color(0,0,0));
+		    modifNomPers.setText(data);	
+		}
+		
+		/**
+		 * 
+		 * Cette méthode permet de désactiver le champ
+		 * @param lbl
+		 * @param modifNomEqui
+		 */
+		public void desactiverChampMail(){
+			modifNomEqui.setEditable(false);
+		    modifNomEqui.setEnabled(false);
+		    modifNomEqui.setBackground(new Color(102, 102, 102));
+		    lblNomEqui.setForeground(new Color(102, 102, 102));
+		    modifNomEqui.setText("");
+		}
+		
+		/**
+		 * 
+		 * Cette méthode permet de désactiver le champ
+		 * @param lbl
+		 * @param modifMembres
+		 */
+		public void desactiverChampPrenom(){
+			modifMembres.setEditable(false);
+		    modifMembres.setEnabled(false);
+		    modifMembres.setBackground(new Color(102, 102, 102));
+		    lblMembres.setForeground(new Color(102, 102, 102));
+		    modifMembres.setText("");
+		}
+		
+		/**
+		 * 
+		 * Cette méthode permet de désactiver le champ
+		 * @param lbl
+		 * @param modifNomPers
+		 */
+		public void desactiverChampNom(){
+			modifNomPers.setEditable(false);
+		    modifNomPers.setEnabled(false);
+		    modifNomPers.setBackground(new Color(102, 102, 102));
+		    lblNom.setForeground(new Color(102, 102, 102));
+		    modifNomPers.setText("");
 		}
 
+		
+		
 		public JPanel getOnglet()
 		{
-			return this.ongletEquipe;
+			JPanel ongletPers = new JPanel();
+			associateLabels();
+			ongletPers.setLayout(new BorderLayout());
+			ongletPers.setName("Personnes");
+			ongletPers.add(getCardModif());
+			return ongletPers;
 		}
+		
+		/**
+		 * 
+		 * @param String name
+		 */
+		private String getChampNom(){
+			return modifNomPers.getText();
+		}
+		
+		/**
+		 * 
+		 * @param String firstname
+		 */
+		private String getChampPrenom(){
+			return modifMembres.getText();
+		}
+		
+		/**
+		 * 
+		 * @param String  email
+		 */
+		private String getChampMail(){
+			return modifNomEqui.getText();
+		}
+		
+		/**
+		 * 
+		 * @param String name
+		 */
+		private void setChampNom(String name){
+			modifNomPers.setText(name);
+		}
+		
+		/**
+		 * 
+		 * @param String firstname
+		 */
+		private void setChampPrenom(String firstname){
+			modifMembres.setText(firstname);
+		}
+		
+		/**
+		 * 
+		 * @param String  email
+		 */
+		private void setChampMail(String email){
+			modifNomEqui.setText(email);
+		}
+		
+		/**
+		 * @return the lblEmail
+		 */
+		private JLabel getLblEmail()
+		{
+			return lblNomEqui;
+		}
+
+		/**
+		 * @param lblEmail the lblEmail to set
+		 */
+		private void setLblEmail(JLabel lblEmail)
+		{
+			this.lblNomEqui = lblEmail;
+		}
+
+		/**
+		 * @return the lblPrenom
+		 */
+		private JLabel getLblPrenom()
+		{
+			return lblMembres;
+		}
+
+		/**
+		 * @param lblPrenom the lblPrenom to set
+		 */
+		private void setLblPrenom(JLabel lblPrenom)
+		{
+			this.lblMembres = lblPrenom;
+		}
+
+		/**
+		 * @return the lblNom
+		 */
+		private JLabel getLblNom()
+		{
+			return lblNom;
+		}
+
+		/**
+		 * @param lblNom the lblNom to set
+		 */
+		private void setLblNom(JLabel lblNom)
+		{
+			this.lblNom = lblNom;
+		}
+		
 	}
